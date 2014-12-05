d073d6296e15a9307d9a0e19280dbc0ec1c5e476
ViliamKopecky <vk@viliamkopecky.cz>
2014-12-05 12:08:32 +0100

    Enable plugin even without present `Tracy\Debugger`

--- plugin.php
+++ plugin.php
@@ -3,21 +3,27 @@
  * Plugin Name: Adminbar environment color
  * Plugin URI: https://github.com/mangoweb/wp-adminbar-environment-color
  * Description: Plugin detects environment via Tracy\Debugger and changes background color of adminbar
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: manGoweb
  * Author URI: http://mangoweb.cz/
  * License: MIT License
  */
 
 function hook_development_wpadminbar_color() {
-	if(!class_exists('Tracy\Debugger')) {
-		return FALSE;
+	$production = TRUE;
+
+	if(class_exists('Tracy\Debugger')) {
+		$production = Tracy\Debugger::$productionMode;
+	} else {
+		$developmentWhitelist = [];
+		if (!isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+			$developmentWhitelist[] = '127.0.0.1';
+			$developmentWhitelist[] = '::1';
+		}
+		$production = !in_array(isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : php_uname('n'), $developmentWhitelist, TRUE);
 	}
 
-	$color = '#234';
-	if(!Tracy\Debugger::$productionMode) {
-		$color = '#A22';
-	}
+	$color = $production ? '#A22' : '#234';
 
 ?>
 <style>
